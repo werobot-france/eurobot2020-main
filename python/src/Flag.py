7055dadee1cd6731822872c9cf51cedbe7b5e151
Matthieu Bessat <spamfree@matthieubessat.fr>
2020-08-30 18:16:46 +0200

    feat: WIP tmp flag

---
+++ python/src/Flag.py
@@ -0,0 +1,41 @@
+from time import sleep
+from .ThreadHelper import Thread
+
+'''
+Abstration of the flag component
+Goal: to enable the flag at the 95'' second
+'''
+class Flag:
+
+  driver = None
+  
+  # the last servo slot of the first slots block
+  servoSlot = 3
+  
+  def __init__(self, container = None):
+    self.logger = container.get('logger').get('Flag')
+    self.driver = container.get('driver')
+    if self.driver == None:
+      self.logger.warn('mocked!')
+
+  def open(self, values):
+    self.driver.setAngle(self.servoSlot, 90)
+    self.logger.info('opened!')
+
+  def close(self, values):
+    self.driver.setAngle(self.servoSlot, 180)
+    self.logger.info('closed!')
+
+  def start(self):
+    self.mainThread = Thread(target=self.run)
+    self.mainThread.start()
+  
+  def stop(self):
+    self.mainThread.stop()
+  
+  def run(self):
+    self.close()
+    self.logger.info('started, ready to fire!')
+    sleep(95)
+    self.open()
+
